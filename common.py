7f86c602f50a1e0533d41427b87f0a49712bff31
luozui <329690971@qq.com>
2019-04-16 18:03:28 +0800

    修改接口和room信息

--- common.py
+++ common.py
@@ -89,8 +89,8 @@ def get_url(url, parameters={}, t_out=3):
 
 
 # from '第一阅览室001' to seat-id = 22558
-ROOM = """ 
-       {"status":"success","data":[{"roomId":41,"room":"第一阅览室","floor":2,"reserved":0,"inUse":0,"away":0,"totalSeats":136,"free":136},{"roomId":12,"room":"第二阅览室中区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":11,"room":"第二阅览室北区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":196,"free":196},{"roomId":13,"room":"第二阅览室南区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":172,"free":172},{"roomId":15,"room":"第十一阅览室中区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":14,"room":"第十一阅览室北区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":188,"free":188},{"roomId":16,"room":"第十一阅览室南区","floor":3,"reserved":0,"inUse":0,"away":0,"totalSeats":156,"free":156},{"roomId":18,"room":"第三阅览室中区","floor":4,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":17,"room":"第三阅览室北区","floor":4,"reserved":0,"inUse":0,"away":0,"totalSeats":148,"free":148},{"roomId":19,"room":"第三阅览室南区","floor":4,"reserved":0,"inUse":0,"away":0,"totalSeats":120,"free":120},{"roomId":21,"room":"第十阅览室中区","floor":4,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":22,"room":"第十阅览室南区","floor":4,"reserved":0,"inUse":0,"away":0,"totalSeats":164,"free":164},{"roomId":35,"room":"第九阅览室中区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":34,"room":"第九阅览室北区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":195,"free":195},{"roomId":36,"room":"第九阅览室南区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":172,"free":172},{"roomId":32,"room":"第四阅览室中区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":31,"room":"第四阅览室北区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":148,"free":148},{"roomId":33,"room":"第四阅览室南区","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":164,"free":164},{"roomId":38,"room":"第五阅览室中区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":8,"room":"第五阅览室北区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":59,"free":59},{"roomId":37,"room":"第五阅览室南区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":173,"free":173},{"roomId":47,"room":"第八阅览室中区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":9,"room":"第八阅览室北区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":204,"free":204},{"roomId":40,"room":"第八阅览室南区","floor":6,"reserved":0,"inUse":0,"away":0,"totalSeats":176,"free":176},{"roomId":27,"room":"第七阅览室中区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":46,"room":"第七阅览室北区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":132,"free":132},{"roomId":28,"room":"第七阅览室南区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":108,"free":108},{"roomId":24,"room":"第六阅览室中区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":48,"free":48},{"roomId":23,"room":"第六阅览室北区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":132,"free":132},{"roomId":25,"room":"第六阅览室南区","floor":7,"reserved":0,"inUse":0,"away":0,"totalSeats":108,"free":108}],"message":"","code":"0"}
+ROOM = """
+       {"status":"success","data":[{"roomId":12,"room":"一层大厅","floor":1,"reserved":0,"inUse":4,"away":0,"totalSeats":169,"free":165},{"roomId":11,"room":"朗读亭","floor":2,"reserved":0,"inUse":0,"away":0,"totalSeats":1,"free":1},{"roomId":3,"room":"三楼天井区","floor":3,"reserved":0,"inUse":40,"away":0,"totalSeats":40,"free":0},{"roomId":4,"room":"四楼天井区","floor":4,"reserved":1,"inUse":39,"away":0,"totalSeats":40,"free":0},{"roomId":1,"room":"五楼天井区","floor":5,"reserved":1,"inUse":74,"away":1,"totalSeats":80,"free":4},{"roomId":6,"room":"五楼教师研究生阅览室","floor":5,"reserved":0,"inUse":0,"away":0,"totalSeats":32,"free":0},{"roomId":2,"room":"五楼自习区","floor":5,"reserved":3,"inUse":242,"away":0,"totalSeats":256,"free":10},{"roomId":5,"room":"六楼自习区","floor":6,"reserved":1,"inUse":240,"away":0,"totalSeats":312,"free":70}],"message":"","code":"0"}
 """
 ROOM = json.loads(ROOM)
 ROOM = ROOM['data']
@@ -101,8 +101,9 @@ def get_seat_id(loc, token):
     print("得到座位号ID...")
     local_room = loc[:-3]
     local_seat = loc[-3:]
+    print(local_room, local_seat)
     room_id = [x for x in ROOM if x["room"] == local_room][0]['roomId']
-    room_layer_url = 'http://seat.ujn.edu.cn/rest/v2/room/layoutByDate/' + str(room_id) + '/2017-01-2' \
+    room_layer_url = 'http://seatlib.hpu.edu.cn/rest/v2/room/layoutByDate/' + str(room_id) + '/2017-01-2' \
                                                                                           '2?token=' + token
     r = get_url(room_layer_url)
     try:
@@ -124,7 +125,7 @@ def get_seat_id(loc, token):
 def get_token(username, password):
     # 登录获取token
     print("正在登陆...")
-    url = 'http://seat.ujn.edu.cn/rest/auth'
+    url = 'http://seatlib.hpu.edu.cn/rest/auth'
     param = {
         'username': username,
         'password': password
